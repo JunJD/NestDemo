c03fc2fc470aa5adc6b8721b8a6d5ca0a62acf8a
dingjunjie <dingjunjie@ypshengxian.com>
2023-02-13 00:23:56 +0800

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthGithubModule } from './auth_github/auth_github.module';
 import { CorsMiddleware } from './middlewares/cors.middlewares';
 import { User } from './entities/user.entity'; //
 import { UsersModule } from './modules/users/users.module';
 @Module({
   imports: [
     UsersModule,
-    AuthGithubModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
@@ -20,9 +18,6 @@ import { UsersModule } from './modules/users/users.module';
       entities: [User],
       synchronize: true,
     }),
-    // TypeOrmModule.forFeature([User, Strategy]),
-    // UserModule,
-    // AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
